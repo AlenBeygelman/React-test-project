97a54fbf11ab234b1d95e943e19bfd5f76477555
Alen Beygelman <alen.beygelman@gmail.com>
2018-11-26 17:27:41 -0800

    add expense summary to dashboard

---
+++ src/selectors/expenses-total.js
@@ -0,0 +1,9 @@
+export default (expenses) => {
+ if (expenses.length === 0) {
+   return 0;
+ }
+ else{
+   const total = expenses.map((expense) => expense.amount).reduce((accumulator, currentValue) => accumulator + currentValue, 0);
+   return total;
+ }
+};
